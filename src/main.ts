fb19ae0eef2aa8462ea5604e0d98d0feb7525818
Wagner Mattei <wagner.mattei@eits.com.br>
2019-05-26 03:18:29 -0300

    Package changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { NgxColorModule } from './app/app.module';
 
 enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(NgxColorModule)
   .catch(err => console.error(err));
 
